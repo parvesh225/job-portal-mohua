6eb0f7105ac0365bf8c62275fcf3ab69c0c80291
parvesh225 <39706951+parvesh225@users.noreply.github.com>
2024-04-04 14:21:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,217 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, Grid, Card, CardContent, Container } from '@mui/material';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="absolute" color="default">
+        <Toolbar>
+          <Grid container justifyContent="space-between" alignItems="center">
+            <Grid item>
+              <img src='./logo.jpeg' width={130} alt="Logo" />
+            </Grid>
+            <Grid item>
+              <Button color="inherit">Login</Button>
+              <Button color="inherit" sx={{ backgroundColor: '#2196f3', color: '#fff' }}>Register</Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+     <Container style={{ maxWidth: '80%', marginTop: '10px', marginBottom:'20px' , textAlign: 'center'}}>
+      <Typography variant="h6" style={{ textAlign: 'center', paddingTop: '100px' }}>
+          Careers at Ministry of Housing and Urban Affairs, Goverment of India
+        </Typography>
+        <Typography variant="body">
+        OPEN POSITIONS : Purely on contract basis
+        </Typography>
+     </Container>
+
+      <Container style={{ maxWidth: '80%', marginTop: '30px' }}>
+        <Grid container spacing={3}>
+          <Grid item xs={4}>
+            <Card style={{ borderLeft: '4px solid #2196f3', backgroundColor:'#EFF6FE'}}>
+              <CardContent>
+                <Typography variant="h6" component="div">
+                  ASP.Net Developers
+                </Typography>
+                <Typography variant="body2" style={{ display: 'flex', justifyContent: 'space-between', marginTop: '3px', marginBottom: '3px' }}>
+                  <span>Experience: 1-5 years</span>
+                  <span>Positions - 20</span>
+                </Typography>
+                <Typography variant="body2" style={{marginBottom: '3px'}} >
+                  Qualification: BE/MCA
+                </Typography>
+                <Typography variant="body2">
+                  Mandatory skill: ASP.Net & SQL
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            <Card style={{ borderLeft: '4px solid #2196f3', backgroundColor:'#EFF6FE'}}>
+              <CardContent>
+                <Typography variant="h6" component="div">
+                  ASP.Net Developers
+                </Typography>
+                <Typography variant="body2" style={{ display: 'flex', justifyContent: 'space-between', marginTop: '3px', marginBottom: '3px' }}>
+                  <span>Experience: 1-5 years</span>
+                  <span>Positions - 20</span>
+                </Typography>
+                <Typography variant="body2" style={{marginBottom: '3px'}} >
+                  Qualification: BE/MCA
+                </Typography>
+                <Typography variant="body2">
+                  Mandatory skill: ASP.Net & SQL
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Card style={{ borderLeft: '4px solid #2196f3', backgroundColor:'#EFF6FE'}}>
+              <CardContent>
+                <Typography variant="h6" component="div">
+                  ASP.Net Developers
+                </Typography>
+                <Typography variant="body2" style={{ display: 'flex', justifyContent: 'space-between', marginTop: '3px', marginBottom: '3px' }}>
+                  <span>Experience: 1-5 years</span>
+                  <span>Positions - 20</span>
+                </Typography>
+                <Typography variant="body2" style={{marginBottom: '3px'}} >
+                  Qualification: BE/MCA
+                </Typography>
+                <Typography variant="body2">
+                  Mandatory skill: ASP.Net & SQL
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Card style={{ borderLeft: '4px solid #2196f3', backgroundColor:'#EFF6FE'}}>
+              <CardContent>
+                <Typography variant="h6" component="div">
+                  ASP.Net Developers
+                </Typography>
+                <Typography variant="body2" style={{ display: 'flex', justifyContent: 'space-between', marginTop: '3px', marginBottom: '3px' }}>
+                  <span>Experience: 1-5 years</span>
+                  <span>Positions - 20</span>
+                </Typography>
+                <Typography variant="body2" style={{marginBottom: '3px'}} >
+                  Qualification: BE/MCA
+                </Typography>
+                <Typography variant="body2">
+                  Mandatory skill: ASP.Net & SQL
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Card style={{ borderLeft: '4px solid #2196f3', backgroundColor:'#EFF6FE'}}>
+              <CardContent>
+                <Typography variant="h6" component="div">
+                  ASP.Net Developers
+                </Typography>
+                <Typography variant="body2" style={{ display: 'flex', justifyContent: 'space-between', marginTop: '3px', marginBottom: '3px' }}>
+                  <span>Experience: 1-5 years</span>
+                  <span>Positions - 20</span>
+                </Typography>
+                <Typography variant="body2" style={{marginBottom: '3px'}} >
+                  Qualification: BE/MCA
+                </Typography>
+                <Typography variant="body2">
+                  Mandatory skill: ASP.Net & SQL
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Card style={{ borderLeft: '4px solid #2196f3',  backgroundColor:'#EFF6FE'}}>
+              <CardContent>
+                <Typography variant="h6" component="div">
+                  ASP.Net Developers
+                </Typography>
+                <Typography variant="body2" style={{ display: 'flex', justifyContent: 'space-between', marginTop: '3px', marginBottom: '3px' }}>
+                  <span>Experience: 1-5 years</span>
+                  <span>Positions - 20</span>
+                </Typography>
+                <Typography variant="body2" style={{marginBottom: '3px'}} >
+                  Qualification: BE/MCA
+                </Typography>
+                <Typography variant="body2">
+                  Mandatory skill: ASP.Net & SQL
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Card style={{ borderLeft: '4px solid #2196f3', backgroundColor:'#EFF6FE'}}>
+              <CardContent>
+                <Typography variant="h6" component="div">
+                  ASP.Net Developers
+                </Typography>
+                <Typography variant="body2" style={{ display: 'flex', justifyContent: 'space-between', marginTop: '3px', marginBottom: '3px' }}>
+                  <span>Experience: 1-5 years</span>
+                  <span>Positions - 20</span>
+                </Typography>
+                <Typography variant="body2" style={{marginBottom: '3px'}} >
+                  Qualification: BE/MCA
+                </Typography>
+                <Typography variant="body2">
+                  Mandatory skill: ASP.Net & SQL
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Card style={{ borderLeft: '4px solid #2196f3', backgroundColor:'#EFF6FE'}}>
+              <CardContent>
+                <Typography variant="h6" component="div">
+                  ASP.Net Developers
+                </Typography>
+                <Typography variant="body2" style={{ display: 'flex', justifyContent: 'space-between', marginTop: '3px', marginBottom: '3px' }}>
+                  <span>Experience: 1-5 years</span>
+                  <span>Positions - 20</span>
+                </Typography>
+                <Typography variant="body2" style={{marginBottom: '3px'}} >
+                  Qualification: BE/MCA
+                </Typography>
+                <Typography variant="body2">
+                  Mandatory skill: ASP.Net & SQL
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Card style={{ borderLeft: '4px solid #2196f3', backgroundColor:'#EFF6FE'}}>
+              <CardContent>
+                <Typography variant="h6" component="div">
+                  ASP.Net Developers
+                </Typography>
+                <Typography variant="body2" style={{ display: 'flex', justifyContent: 'space-between', marginTop: '3px', marginBottom: '3px' }}>
+                  <span>Experience: 1-5 years</span>
+                  <span>Positions - 20</span>
+                </Typography>
+                <Typography variant="body2" style={{marginBottom: '3px'}} >
+                  Qualification: BE/MCA
+                </Typography>
+                <Typography variant="body2">
+                  Mandatory skill: ASP.Net & SQL
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
